9a84b8acd8bff243f7f1fd102ce327fcfedf5bb6
Davebowman276 <Davebowman276@gmail.com>
2019-12-10 15:25:03 -0500

    added an ingredient bean

---
+++ CookingDaddy/src/main/java/com/revature/beans/Ingredient.java
@@ -0,0 +1,112 @@
+package com.revature.beans;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Ingredient {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="ingredient")
+	@SequenceGenerator(name="ingredient", sequenceName="ingredient_seq", allocationSize=1)
+	private Integer id;
+	
+	@ManyToOne(fetch=FetchType.EAGER, cascade=CascadeType.ALL)
+	@JoinColumn(name="category_id")
+	private Category category;
+	@ManyToOne(fetch=FetchType.EAGER, cascade=CascadeType.ALL)
+	@JoinColumn(name="quality_id")
+	private Quality quality;
+	@ManyToOne(fetch=FetchType.EAGER, cascade=CascadeType.ALL)
+	@JoinColumn(name="flavor_id")
+	private Flavor flavor;
+	private String name;
+	private Integer inventory;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	public Quality getQuality() {
+		return quality;
+	}
+	public void setQuality(Quality quality) {
+		this.quality = quality;
+	}
+	public Flavor getFlavor() {
+		return flavor;
+	}
+	public void setFlavor(Flavor flavor) {
+		this.flavor = flavor;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getInventory() {
+		return inventory;
+	}
+	public void setInventory(Integer inventory) {
+		this.inventory = inventory;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((inventory == null) ? 0 : inventory.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ingredient other = (Ingredient) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (inventory == null) {
+			if (other.inventory != null)
+				return false;
+		} else if (!inventory.equals(other.inventory))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Ingredient [id=" + id + ", name=" + name + ", inventory=" + inventory + "]";
+	}
+	
+
+
+
+}
