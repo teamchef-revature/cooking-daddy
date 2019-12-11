42dfb16cf5fb4ca45a62e2413a40296ebbacba6f
Davebowman276 <Davebowman276@gmail.com>
2019-12-10 19:48:34 -0500

    added ingredient DAO

---
+++ CookingDaddy/src/main/java/com/revature/data/IngredientDAO.java
@@ -0,0 +1,15 @@
+package com.revature.data;
+
+import java.util.Set;
+
+import com.revature.beans.Ingredient;
+
+public interface IngredientDAO {
+	
+	Integer addIngredient(Ingredient i);
+	Set<Ingredient> getIngredients();
+	Ingredient getIngredient(Integer i);
+	public void updateIngredient(Ingredient i);
+	public void deleteIngredient(Ingredient i);
+
+}
