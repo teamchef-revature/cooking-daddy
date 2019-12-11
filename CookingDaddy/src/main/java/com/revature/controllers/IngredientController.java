f5d4ec9e0917340d4fbcaa3234acfb35b8e95602
Davebowman276 <Davebowman276@gmail.com>
2019-12-11 12:55:00 -0500

    added controller annotation to ingredientController

--- CookingDaddy/src/main/java/com/revature/controllers/IngredientController.java
+++ CookingDaddy/src/main/java/com/revature/controllers/IngredientController.java
@@ -3,14 +3,20 @@ package com.revature.controllers;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.beans.Ingredient;
 import com.revature.data.IngredientDAO;
 
+
+@Controller
+@RequestMapping(value="/ingredients")
 public class IngredientController {
 	
 	@Autowired
