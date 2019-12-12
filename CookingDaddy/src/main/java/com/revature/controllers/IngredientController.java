a2b211719befea754a01fe114a8b752515349c47
snicholes <s.k.nicholes@gmail.com>
2019-12-12 14:40:18 -0500

    fixing more errors, finishing registering user

--- CookingDaddy/src/main/java/com/revature/controllers/IngredientController.java
+++ CookingDaddy/src/main/java/com/revature/controllers/IngredientController.java
@@ -3,19 +3,19 @@ package com.revature.controllers;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.beans.Ingredient;
 import com.revature.data.IngredientDAO;
 
 
-@Controller
+@RestController
 @CrossOrigin(origins="http://localhost:4200")
 @RequestMapping(value="/ingredients")
 public class IngredientController {
