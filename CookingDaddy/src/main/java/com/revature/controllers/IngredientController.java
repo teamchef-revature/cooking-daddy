e7aadd44fd0fab8a3e764cc32c374f94346d0106
Davebowman276 <Davebowman276@gmail.com>
2019-12-12 14:57:26 -0500

    uhh

--- CookingDaddy/src/main/java/com/revature/controllers/IngredientController.java
+++ CookingDaddy/src/main/java/com/revature/controllers/IngredientController.java
@@ -10,13 +10,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.beans.Ingredient;
 import com.revature.data.IngredientDAO;
 
 
-@Controller
+@RestController
 @CrossOrigin(origins="http://localhost:4200")
 @RequestMapping(value="/ingredients")
 public class IngredientController {
@@ -25,13 +26,13 @@ public class IngredientController {
 	private IngredientDAO id;
 	
 	@GetMapping
-	//@ResponseBody
+	@ResponseBody
 	public Set<Ingredient> getAll(){
 		return id.getIngredients();
 	}
 	
 	@GetMapping("{steve}")
-	//@ResponseBody
+	@ResponseBody
 	public Ingredient getIngredient(@PathVariable("steve") Integer bob) {
 		return id.getIngredient(bob);
 	}
