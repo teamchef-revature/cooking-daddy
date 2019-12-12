fef60d70617b53df5d3d54fcd5d48b4a4ec8f61a
Ryan Marinoff <ryan.marinoff@gmail.com>
2019-12-12 10:10:24 -0500

    working towards completion

--- Angular/cookingdaddy/src/app/app-routing.module.ts
+++ Angular/cookingdaddy/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { IngredientComponent } from './fridge/ingredient/ingredient.component';
-
+import { IngredientControllerComponent } from './admin/ingredient-controller/ingredient-controller.component';
 
 const routes: Routes = [
   {
@@ -17,6 +17,10 @@ const routes: Routes = [
   {
     path: 'ingredient',
     component: IngredientComponent
+  },
+  {
+    path: 'admin/ingredients',
+    component: IngredientControllerComponent
   }
 ];
 
--- CookingDaddy/src/main/java/com/revature/controllers/IngredientController.java
+++ CookingDaddy/src/main/java/com/revature/controllers/IngredientController.java
@@ -4,6 +4,7 @@ import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import com.revature.data.IngredientDAO;
 
 
 @Controller
+@CrossOrigin(origins="http://localhost:4200")
 @RequestMapping(value="/ingredients")
 public class IngredientController {
 	
