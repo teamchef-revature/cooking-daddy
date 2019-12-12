d4f6d44376768ab710b351c9977c6d8b3c6ebd6c
Davebowman276 <Davebowman276@gmail.com>
2019-12-12 12:41:14 -0500

    added ingredient service layer

---
+++ CookingDaddy/src/main/java/com/revature/service/IngredientService.java
@@ -0,0 +1,15 @@
+package com.revature.service;
+
+import java.util.Set;
+
+import com.revature.beans.Ingredient;
+
+public interface IngredientService {
+	
+	Integer addIngredient(Ingredient i);
+	Set<Ingredient> getIngredients();
+	Ingredient getIngredient(Integer i);
+	Ingredient updateIngredient(Ingredient i);
+	public void deleteIngredient(Ingredient i);
+
+}
