c08abfe170a78096031932c89a89bfe847102009
Ryan Marinoff <ryan.marinoff@gmail.com>
2019-12-12 19:43:38 -0500

    Addeded more code

--- CookingDaddy/src/main/java/com/revature/controllers/AdminController.java
+++ CookingDaddy/src/main/java/com/revature/controllers/AdminController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +33,11 @@ public class AdminController {
 	public ResponseEntity<Ingredient> getIngredient(@PathVariable Integer id) {
 		return ResponseEntity.ok(ingredientDAO.getIngredient(id));
 	}
+	@PostMapping(value="/ingredient")
+	public ResponseEntity<Ingredient> addIngredient(@RequestBody Ingredient ingredient) {
+		ingredientDAO.addIngredient(ingredient);
+		return ResponseEntity.status(201).body(ingredient);
+	}
 	
 	@GetMapping(value="/category")
 	public ResponseEntity<Set<Category>> getCategories() {
@@ -39,6 +46,11 @@ public class AdminController {
 	@GetMapping(value="/category/{id}")
 	public ResponseEntity<Category> getCategory(@PathVariable Integer id) {
 		return ResponseEntity.ok(ingredientDAO.getCategory(id));
+	}
+	@PostMapping(value="/category")
+	public ResponseEntity<Category> addCategory(@RequestBody Category category) {
+		ingredientDAO.addCategory(category);
+		return ResponseEntity.status(201).body(category);
 	}
 	
 	@GetMapping(value="/flavor")
@@ -49,6 +61,11 @@ public class AdminController {
 	public ResponseEntity<Flavor> getFlavor(@PathVariable Integer id) {
 		return ResponseEntity.ok(ingredientDAO.getFlavor(id));
 	}
+	@GetMapping(value="/flavor")
+	public ResponseEntity<Flavor> addFlavor(@RequestBody Flavor flavor) {
+		ingredientDAO.addFlavor(flavor);
+		return ResponseEntity.status(201).body(flavor);
+	}
 	
 	@GetMapping(value="/quality")
 	public ResponseEntity<Set<Quality>> getQualities() {
@@ -57,5 +74,10 @@ public class AdminController {
 	@GetMapping(value="/quality/{id}")
 	public ResponseEntity<Quality> getQuality(@PathVariable Integer id) {
 		return ResponseEntity.ok(ingredientDAO.getQuality(id));
+	}
+	@GetMapping(value="/quality")
+	public ResponseEntity<Quality> addQuality(@RequestBody Quality quality) {
+		ingredientDAO.addQuality(quality);
+		return ResponseEntity.status(201).body(quality);
 	}
 }
