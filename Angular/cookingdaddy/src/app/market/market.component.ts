c825d742b1cb8d913ffc4f36f442fc1e4fcecad8
Davebowman276 <Davebowman276@gmail.com>
2019-12-14 18:32:24 -0500

    uhhh

--- Angular/cookingdaddy/src/app/market/market.component.ts
+++ Angular/cookingdaddy/src/app/market/market.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./market.component.css']
 })
 export class MarketComponent implements OnInit {
-
+  public name = 'trade';
   constructor() { }
 
   ngOnInit() {
