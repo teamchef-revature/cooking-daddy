b018ef4c540c32a9a5ecad83e8f07630e596a60f
Davebowman276 <Davebowman276@gmail.com>
2019-12-15 13:35:17 -0500

    add and remove from basket

--- Angular/cookingdaddy/src/app/marketplace/basket/basket.component.ts
+++ Angular/cookingdaddy/src/app/marketplace/basket/basket.component.ts
@@ -3,6 +3,7 @@ import { MarketServiceService } from '../market-service.service';
 import { Person } from 'src/app/shared/person/person';
 import { PersonIngredient } from 'src/app/shared/personIngredient/person-ingredient';
 import { PersonEquipment } from 'src/app/shared/equipment/person-equipment';
+import { PersonService } from 'src/app/shared/person/person.service';
 
 @Component({
   selector: 'app-basket',
@@ -12,15 +13,16 @@ import { PersonEquipment } from 'src/app/shared/equipment/person-equipment';
 export class BasketComponent implements OnInit {
   @Input() holder: Person;
   choice: number;
-  constructor(private basketHolder: MarketServiceService) {
+  activePerson: Person;
+  constructor(private basketHolder: MarketServiceService, active: PersonService) {
     if (!this.holder) {
       this.holder = basketHolder.getBasket();
       this.choice = 1;
+      this.activePerson = active.getPerson();
     }
   }
   ngOnInit() {
     this.choice = 1;
-    this.basketHolder.bkhsub.subscribe(resp => this.holder as Person);
   }
   chooseIng() {
     this.choice = 1;
@@ -30,10 +32,14 @@ export class BasketComponent implements OnInit {
   }
   onIngClick(ping: PersonIngredient) {
     if (ping.inventory === 1) {
-      const ingredients: PersonIngredient[] = this.holder.ingredients.filter(element => !ping);
+      const ingredients: PersonIngredient[] = this.holder.ingredients.filter(element => element !== ping);
       this.holder.ingredients = ingredients;
+      this.basketHolder.restoreItem(ping, this.activePerson);
+
     } else {
       ping.inventory--;
+      console.log(this.activePerson);
+      this.basketHolder.restoreItem(ping, this.activePerson);
     }
   }
 }
