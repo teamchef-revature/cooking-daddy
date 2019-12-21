47fbe7655efb124930491e975bf03ec86e90184e
Davebowman276 <Davebowman276@gmail.com>
2019-12-21 17:06:58 -0500

    added meal pipe for search

---
+++ Angular/cookingdaddy/src/app/meal.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { MealPipe } from './meal.pipe';
+
+describe('MealPipe', () => {
+  it('create an instance', () => {
+    const pipe = new MealPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
