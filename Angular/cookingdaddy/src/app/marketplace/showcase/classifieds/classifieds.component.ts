270d5e3126e50a3dd2fc74f129d0a202c947dda0
Andrew Taylor <initialsaet@gmail.com>
2019-12-20 23:23:39 -0500

    forgot module showed some initial components

---
+++ Angular/cookingdaddy/src/app/marketplace/showcase/classifieds/classifieds.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-classifieds',
+  templateUrl: './classifieds.component.html',
+  styleUrls: ['./classifieds.component.css']
+})
+export class ClassifiedsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
